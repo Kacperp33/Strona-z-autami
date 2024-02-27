1c197b34392934a8cb23e4dd6438cceec994734e
Kacperp33 <kacper33p@gmail.com>
2024-02-27 11:07:00 +0100

    Drugi commit
    Dodanie sortowania bąbelkowego i prototypu menu

--- script.js
+++ script.js
@@ -46,5 +46,47 @@ function akcja(){
 
 let interwał = setInterval(akcja, 5000)
 
+let wszystkieliczby =[];
+
+function dodajliczbe() {
+
+    let liczbyinput = document.getElementById("liczby"); // pobranie liczb z inputa które potem trzeba przenieś do nowej zmiennej
+    let liczby = liczbyinput.value; // definicja zmiennej liczby i dodanie doniej wartości z inputa
+
+    wszystkieliczby.push(parseInt(liczby)); // wysłanie do tablicy liczb z zmiennej "liczby"
+
+    console.log("Wszystkie liczby:", wszystkieliczby) // wyświetlenie w konsoli
+
+    liczbyinput.value = ""; // wyczyszczenie inputa co każde dodanie liczby
+}
+
+function licz() {
+
+    let n = wszystkieliczby.length; // dodanie zmiennej do definicji długości tablicy
+
+    console.log(n); // wyświetlenei w konsoli długości tablicy 
+
+    for(let i=0; i<n; i++){
+        for(let j = 1; j < n ; j++){
+            if(wszystkieliczby[j -1] > wszystkieliczby[j]) {
+                let temp = wszystkieliczby[j - 1];
+                wszystkieliczby[j - 1] = wszystkieliczby[j];
+                wszystkieliczby[j] = temp;
+            
+            }
+        }
+    }
 
 
+
+
+    console.log(wszystkieliczby);
+
+
+    document.getElementById("posrtowane").innerHTML = wszystkieliczby;
+
+    alert("Kys");
+
+
+
+}
